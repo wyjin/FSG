d6173f3020d2e9c10cfbc766ffe41354963ac236
mkonto <mkonto@umich.edu>
2018-11-03 19:07:59 -0400

    sadness.jpg

--- main.cpp
+++ main.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	// does everything
+	void run() {
+		build_application_mapping();
+		while(true) {
+			// listens for file system read/writes
+			pair<pid_t, string> access = get_access();
+			string app = applications[access.first];
+			string file = access.second;
+			accesses[app].add(file);
+			cout << app << " accessed file " << file << endl;
+		}
+	}
+private:
+	void build_application_mapping() {
+		// get a list of pid's
+	}
+
+	pair<pid_t, string> get_access();
+
+	// maps pid to application name
+	unordered_map<pid_t, string> applications;
+
+	// maps applications to which files they accessed
+	unordered_map<string, unordered_set<string>> accesses;
+
+	// maps pid to parent pids (if parent is 0, it's a top level process)
+	unordered_map<pid_t, pid_t> parents;
+
+	unordered_set<string> files_to_track;
+}
+
 void parse_command_line(int argc, char *argv[],vector<string> & files_to_track) {
 	static struct option longopts[] = {
 		{ "verbose",		no_argument,		nullptr,	'v' },
@@ -23,5 +62,7 @@ void parse_command_line(int argc, char *argv[],vector<string> & files_to_track) 
 int main(int argc, char** argv) {
   vector<string> files_to_track;
   parse_command_line(argc, argv, files_to_track);
+  Solution s;//TODO: pass this what it needs from command line
+  s.run();
   return 0;
 }
