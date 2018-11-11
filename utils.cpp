fd837e51aa230254e7e2c8f6a08c567e31eb5214
wyjin <22771792+wyjin@users.noreply.github.com>
2018-11-10 22:39:03 -0500

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -24,19 +24,18 @@ void pid2name(uint32_t pid, char* name_buf) {
     pclose(fp);
 }
 
-void trace_pid(uint32_t pid, char* callstack_buf) {
-    char comm_buf[32] = {'\0'};
-    
-    sprintf(comm_buf, "pstree -sup %d", pid);
-    FILE *fp;
-    fp = popen(comm_buf, "r");
+void trace_pid(uint32_t pid) {
+    char buf[32] = {'\0'};
+    sprintf(buf, "pstree -sup %d", pid);
+    FILE *fp = popen(buf, "r");
     if (fp == NULL) {
-        printf("error when trying to get process name for pid %d", pid);
+        printf("error when trying to find process with pid %d", pid);
         exit(1);
     }
-    if(fgets(callstack_buf, sizeof(callstack_buf) - 1, fp) == NULL) {
-        printf("error copying name for the call stack for pid %d", pid);
-        exit(1);
+    std::string result;
+    while (fgets(buf, sizeof(buf)-1, fp)) {
+        result += buf;
     }
+    std::cout << result << std::endl;
     pclose(fp);
 }
