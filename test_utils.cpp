6eae4548f77f2fa5c73a647b0017347cea3e4453
mkonto <mkonto@umich.edu>
2018-11-11 18:00:50 -0500

    Greatly simplifies Makefile

--- test_utils.cpp
+++ test_utils.cpp
@@ -2,14 +2,19 @@
 #include<string>
 #include<vector>
 #include "utils_real_time.hpp"
+
 using namespace std;
 
 int main() {
-    string info = "systemd(1)---kdeinit5(736,wnyjin)---ksmserver(799)---plasmashell(868)---dolphin(1240)---konsole(1252)---zsh(1307)---vim(4201)";
+    string info = 
+    	"systemd(1)---kdeinit5(736,wnyjin)---"
+    	"ksmserver(799)---plasmashell(868)---"
+    	"dolphin(1240)---konsole(1252)---"
+    	"zsh(1307)---vim(4201)";
     vector<string> procs = split(info, "---");
-    for (auto&& i :procs) {
-        cout << i << endl;
-        cout << print_info(i) << endl;
+    for (const string& proc : procs) {
+        cout << proc << endl;
+        cout << format_info(proc) << endl;
     }
-    
+    return 0;
 }
