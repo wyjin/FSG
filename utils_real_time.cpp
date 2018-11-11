297f09431b72075cfa3b45acbd9ce0b5f5705fb0
W.Jin <wnyjin@aurum.local>
2018-11-11 15:09:18 -0500

    fixed errors

--- utils_real_time.cpp
+++ utils_real_time.cpp
@@ -43,10 +43,12 @@ std::string trace_pid(uint32_t pid) {
 std::vector<std::string> split(const std::string& str, const std::string & delim) {
     std::size_t current, previous = 0;
     current = str.find(delim);
+    cont = std::vector<std::string>;
     while (current != std::string::npos) {
         cont.push_back(str.substr(previous, current - previous));
         previous = current + 1;
         current = str.find(delim, previous);
     }
     cont.push_back(str.substr(previous, current - previous));
+    return cont;
 }
