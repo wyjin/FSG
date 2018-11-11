eb73a91040ee87b24a640641f6e76e6f6c814f95
W.Jin <wnyjin@aurum.local>
2018-11-11 16:10:00 -0500

    added print_info function.

--- utils_real_time.cpp
+++ utils_real_time.cpp
@@ -1,26 +1,19 @@
 #include "utils_real_time.hpp"
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/fanotify.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <boost/algorithm/string.hpp>
 
-void pid2name(uint32_t pid, char* name_buf) {
-    char comm_buf[32] = {0};
-    sprintf(comm_buf, "/bin/cat /proc/%d/comm", pid);
-    FILE *fp;
-    fp = popen(comm_buf, "r");
-    if (fp == NULL) {
-        printf("error when trying to get process name for pid %d", pid);
-        exit(1);
-    }
-    if(fgets(name_buf, sizeof(name_buf) - 1, fp) == NULL) {
-        printf("error copying name for pid %d", pid);
-        exit(1);
-    }
-    pclose(fp);
-}
+using namespace std;
 
-std::vector<std::string> trace_pid(uint32_t pid) {
+vector<string> trace_pid(uint32_t pid) {
     char buf[32] = {0};
     sprintf(buf, "pstree -sup %d", pid);
     FILE *fp = popen(buf, "r");
@@ -28,28 +21,46 @@ std::vector<std::string> trace_pid(uint32_t pid) {
         printf("error when trying to find process with pid %d", pid);
         exit(1);
     }
-    std::string result;
+    string result;
     while (fgets(buf, sizeof(buf)-1, fp)) {
         result += buf;
     }
     pclose(fp);
-    std::vector<std::string> proc_name = split(result, "---");
+    vector<string> proc_name = split(result, "---");
     for (auto&& i: proc_name) {
-        std::cout << i << std::endl;
+        cout << i << endl;
     }
     return proc_name;
 }
 
-std::vector<std::string> split(const std::string& str, const std::string &delim) {
-    std::size_t current, previous = 0;
-    std::size_t pattern_len = delim.length();
+vector<string> split(const string& str, const string &delim) {
+    size_t current, previous = 0;
+    size_t pattern_len = delim.length();
     current = str.find(delim);
-    std::vector<std::string> cont;
-    while (current != std::string::npos) {
+    vector<string> cont;
+    while (current != string::npos) {
         cont.push_back(str.substr(previous, current - previous));
         previous = current + pattern_len;
         current = str.find(delim, previous);
     }
     cont.push_back(str.substr(previous, current - previous));
     return cont;
+}
+
+string print_info(const string &proc_info) {
+    // the info string should look like proc_name(pid, user)
+    size_t start_paren = proc_info.find('(');
+    size_t end_paren = proc_info.find(')');
+    string proc_name = proc_info.substr(0, start_paren);
+    string info_str = "process \"" + proc_name + "\"";
+    string proc_data = proc_info.substr(start_paren + 1, end_paren - start_paren - 1);
+    size_t pos = proc_data.find(',');
+    if (pos != string::npos) {
+        string pid = proc_data.substr(0, pos);
+        string user = proc_data.substr(pos+1, proc_data.length() );
+        info_str += " (pid: " + string(pid) + ")" + " started by user: " + user;
+    } else {
+        info_str += " (pid: " + string(proc_data) + ")";
+    }
+    return info_str;
 }
