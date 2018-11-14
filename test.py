2012eb46b9a4207fe8b4f01ab07c666f2e96dd63
mkonto <mkonto@umich.edu>
2018-11-14 12:01:55 -0500

    Simplifies test, removes redundant print

--- test.py
+++ test.py
@@ -1,32 +1,20 @@
 import os, sys, time
 
 def main():
-    if len(sys.argv) != 3:
-        print('Usage: test.py [r/s] <directory/to/access>')
+    if len(sys.argv) != 2:
+        print('Usage: test.py <directory/to/access>')
         sys.exit(1)
-    mode = sys.argv[1]
-    if mode not in ['s', 'r']:
-        print("Mode must be one of s or r")
-        sys.exit(1)
-    path = sys.argv[2]
+    path = sys.argv[1]
     if not os.path.isdir(path):
         print("Only test on directories")
         sys.exit(1)
 
-    if mode == 'r':
-        while True:
-            for filename in sorted(os.listdir(path)):
-                if not os.path.isdir(filename):
-                    with open(path + '/' + filename, 'r') as openedfile: 
-                        pass
-    else:
+    while True:
         for filename in sorted(os.listdir(path)):
             if not os.path.isdir(filename):
-                # you need to write with open() for it to work, so we can prolly just 
-                # combine r and s into one mode and sleep for like a second in a while(True) loop
-                with open(path + '/' + filename, 'r') as openedfile:  # memory problem but who cares?
-                    print(filename, " is opened")
-                    time.sleep(5)
+                with open(path + '/' + filename, 'r') as openedfile:
+                    time.sleep(1)
+                    pass
 
 if __name__ == '__main__':
     main()
